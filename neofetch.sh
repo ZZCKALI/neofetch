2d717de947bf689a03d8a310b8d1731bed18d301
ZZCKALI <ZZCKALI@163.com>
2025-07-09 11:25:41 +0800

    Create neofetch.sh

---
+++ neofetch.sh
@@ -0,0 +1,19 @@
+#安装neofetch
+pkg update 
+pkg install neofetch -y
+#neofetch显示kali
+neofetch --ascii_distro kali -l #-l只显示图标
+#显示ubuntu
+neofetch --ascii_distro ubuntu -l
+#显示arch
+neofetch --ascii_distro arch -l
+#显示linux
+neofetch --ascii_distro linux -l
+#显示windows
+neofetch --ascii_distro windows -l
+#显示redhat
+neofetch --ascii_distro redhat -l
+#显示Debian
+neofetch --ascii_distro debian -l
+#显示本机信息
+neofetch
